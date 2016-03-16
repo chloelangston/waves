59c1ac448f59874201f625c86255e4f3c8019a92
Chloe Langston <chloealangston@gmail.com>
2016-03-16 16:05:40 -0700

    refactor and moon

--- sketch.js
+++ sketch.js
@@ -4,13 +4,15 @@ var tempY = 0;
 micOn = false;
 
 function setup() {
-    createCanvas(710, 400);
+    createCanvas(910, 500);
     
     button = createButton('interact');
     button.mousePressed(micStart);
     micLevel = 0;
     randomBox = floor(random(width/10))*10;
     console.log(randomBox);
+    
+    drawStars();
     
 }
 
@@ -32,6 +34,26 @@ function draw() {
     }
     
     background(51);
+    
+    drawMoon();
+    
+    
+    var noiseMin = 0;
+    var noiseMax = 1;
+    
+    for (var i = height/2; i < height; i+= 20){
+        drawWaves(0, width, i, i+100, noiseMin, noiseMax, 10);
+        noiseMin += 0.1;
+        noiseMax -= 0.1;
+    }
+    
+//    drawWaves(300, width, 300, 400, 0.2, 0.9, 20);
+//    drawWaves(20, width-80, 250, 300, 0.2, 0.8, 25);
+    
+}
+
+function drawWaves(start, end, waveMin, waveMax, noiseMin, noiseMax, increment){
+    noStroke();
     fill(255);
     // We are going to draw a polygon out of the wave points
     beginShape(); 
@@ -40,50 +62,67 @@ function draw() {
     //var xoff = yoff; // Option #2: 1D Noise
 
     // Iterate over horizontal pixels
-    for (var x = 0; x <= width; x += 10) {
+    for (var x = 0; x <= end; x += increment) {
         // Calculate a y value according to noise, map to 
 
         // Option #1: 2D Noise
-        var y = map(noise(xoff, yoff), 0, 1, 200,300);
-
+        var y = map(noise(xoff, yoff), noiseMin, noiseMax, waveMin, waveMax);
+        
         // Option #2: 1D Noise
         // var y = map(noise(xoff), 0, 1, 200,300);
-//        if (x < 1){
-//            boatX = x;
-//            boatY = y-20;
-//        }
-        
-//        console.log(x);
-        if(x == randomBox){
-            fill(100);
-            rectMode(CENTER);
-            rect(randomBox, y-20, 80, 40);
-            tempY = y;
+
+        if(x == randomBox && waveMin == (height/2)+20){
+            drawBoat(randomBox, y);
         }
         
-//        if (tempY > 0) {
-            if(x == (randomBox+10)){
-//              console.log("inside plus 10");
-                if(y < tempY){
-    //                console.log("should shift right");
-    //                boatY = y;
-                    randomBox = x;
-                }
+        if(x == (randomBox+10)){
+            if(y < tempY){
+                randomBox = x;
             }
-//        }
+        }
         
         var blue = color('rgba(163, 196, 203, 0.5)')
         fill(blue);
-            
-
+        
         // Set the vertex
-        vertex(x, y); 
+        vertex(x+start, y); 
         // Increment x dimension for noise
-        xoff += 0.01 + micLevel;
+        xoff += 0.005 + micLevel;
     }
     // increment y dimension for noise
-    yoff += 0.01;
-    vertex(width, height);
-    vertex(0, height);
+    yoff += 0.001;
+    vertex(end, height);
+    vertex(start, height);
     endShape(CLOSE);
+}
+
+function drawBoat(randomBox, y){
+    fill(100);
+    rectMode(CENTER);
+    rect(randomBox, y-20, 80, 40);
+    tempY = y;
+}
+
+function drawMoon(){
+    var radius = 80;
+    var h = 50;
+    for (var r = radius; r > 0; --r) {
+//        console.log(h);
+        noStroke();
+        fill(h);
+        ellipse(100, 80, r, r);
+        h+= 2;
+    }
+}
+
+function drawStars(){
+    console.log("ehll");
+    for(var j = 0; j < width; j++){
+
+        var starsX = random(width);
+        var starsY = random(height/2);
+        var starColor = color('rgba(255, 255, 255, 0.5)')
+        fill(starColor);
+        ellipse(starsX, starsY, 2, 2);
+    }
 }
